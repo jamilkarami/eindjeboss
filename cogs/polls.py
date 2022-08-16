3d363b78c1b84906491727098e3d4409b1f22979
Jamil Karami <jamil_karami@hotmail.com>
2022-08-16 11:06:05 +0200

    Add daily cat pics and minor improvements

--- cogs/polls.py
+++ cogs/polls.py
@@ -3,9 +3,14 @@ from discord.ext import commands
 from discord import app_commands
 from discord.ui import Button, View, Modal, TextInput
 from vars.eind_vars import *
+import logging
 
 
 class Polls(commands.Cog):
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.info(f"{__name__} Cog is ready")
 
     class poll_modal(Modal, title="Create a Poll"):
         poll_title = TextInput(label = "Poll Title", style=discord.TextStyle.short, placeholder="Knitting session with your grandma")
--- cogs/reddit.py
+++ cogs/reddit.py
@@ -3,21 +3,36 @@ from discord.ext import commands
 from discord import app_commands
 from vars.eind_vars import *
 import re
-import praw
+import asyncpraw
 import os
 from vars.eind_vars import *
+import logging
+import dateparser
+import time
+from dotenv import load_dotenv
+import asyncio
+from vars.periodic_reminders import TOP_REDDIT_CAT_DT
 
 SUBREDDIT_REGEX = "(?<!reddit.com)\/r\/[a-zA-Z0-9]{3,}"
+CHANNEL_ID = int(os.getenv("ANIMALS_CHANNEL_ID"))
 
 class Reddit(commands.Cog):
 
-    reddit = praw.Reddit(
+    reddit = asyncpraw.Reddit(
         client_id=os.getenv("REDDIT_ID"),
         client_secret=os.getenv("REDDIT_SECRET"),
         user_agent=REDDIT_USER_AGENT
     )
 
-    def __init__(self, bot):
+    load_dotenv()
+    CAT_CHAN_ID = os.getenv("ANIMALS_CHANNEL_ID")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.info(f"{__name__} Cog is ready")
+        await self.schedule_cat_pic()
+
+    def __init__(self, bot : discord.Client):
         self.bot = bot
 
     @commands.Cog.listener()
@@ -31,29 +46,38 @@ class Reddit(commands.Cog):
         matches = re.findall(SUBREDDIT_REGEX, message_content)
 
         if matches:
-            await self.handle_reddit_matches(matches, message)
+            await message.reply(self.get_reddit_matches(matches))
         
         return
 
-    @app_commands.command(name="cats", description="Gets a random cat post from /r/cats")
-    async def cats(self, interaction : discord.Interaction):
-        post = next(iter(self.reddit.subreddit("cats").top("day", limit=1)))
+    async def schedule_cat_pic(self):
+        cat_time = dateparser.parse(TOP_REDDIT_CAT_DT, settings={'PREFER_DATES_FROM': 'future'})
+        if cat_time.timestamp() < time.time():
+            cat_time = dateparser.parse(f"tomorrow {TOP_REDDIT_CAT_DT}", settings={'PREFER_DATES_FROM': 'future'})
+            
+        wait_time = cat_time.timestamp() - time.time()
 
+        channel = await self.bot.fetch_channel(CHANNEL_ID)
+
+        logging.info(f"Will send cat pic in {wait_time} seconds")
+        await asyncio.sleep(wait_time)
+
+        post = next(iter(self.reddit.subreddit("cats").top("day", limit=1)))
         title=f"**Top post on /r/cats today: {post.title}**"
         description=post.url
         payload = f"{title}\n{description}"
-        await interaction.response.send_message(payload)
+
+        await channel.send(payload)
         return
     
-    async def handle_reddit_matches(self, matches, message):
+    def get_reddit_matches(self, matches):
         plural = "s" if len(matches) > 1 else ""
         payload = f"Found {len(matches)} subreddit link{plural} in your message:\n"
 
         for match in matches:
             payload = payload + f"https://www.reddit.com{match}\n"
         
-        await message.reply(payload)
-        return
+        return payload
 
 async def setup(bot):
     await bot.add_cog(Reddit(bot))
