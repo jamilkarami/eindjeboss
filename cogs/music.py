b2f4dda3162eef16456183036314c0a803c98e5a
Jamil Karami <jamil_karami@hotmail.com>
2022-08-31 09:46:44 +0200

    Add fixes and optimizations

--- cogs/music.py
+++ cogs/music.py
@@ -43,7 +43,7 @@ class Music(commands.Cog):
                 spotify_act = activity
 
         if spotify_act is None:
-            await interaction.response.send_message("You are not currently listening to anything on Spotify or you haven't connected Discord to your Spotify account.")
+            await interaction.response.send_message("You are not currently listening to anything on Spotify or you haven't connected Discord to your Spotify account.", ephemeral=True)
             return
 
         try:
