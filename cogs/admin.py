edb985b5d788f0ff432bd92ec2e5098986b98d6a
Jamil Karami <jamil_karami@hotmail.com>
2023-06-01 17:51:42 +0200

    Hotfix modmail channel

--- cogs/admin.py
+++ cogs/admin.py
@@ -137,7 +137,8 @@ class Admin(commands.Cog):
 
     @app_commands.command(name="modmail")
     async def modmail(self, intr: discord.Interaction):
-        await intr.response.send_modal(self.TicketModal(self.bot, self.tickets))
+        await intr.response.send_modal(
+            self.TicketModal(self.bot, self.tickets))
         lg.info("Sent ticket modal to %s", intr.user.display_name)
 
     @app_commands.command(name="opentickets")
@@ -296,7 +297,7 @@ class Admin(commands.Cog):
                                 max_length=1024)
 
         async def on_submit(self, intr: discord.Interaction):
-            ticket_channel_id = self.bot.get_setting("modmail_channel")
+            ticket_channel_id = self.bot.get_setting("modmail_channel", None)
             ticket_id = str(uuid.uuid1())[:5]
 
             tick_type = "report" if self.message else "ticket"
