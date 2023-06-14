02243f8f3a7c9d1a128782d466fe2f900f70644c
Jamil Karami <jamil_karami@hotmail.com>
2023-06-14 15:37:21 +0200

    Add message link to exception handling message

--- cogs/exceptionhandler.py
+++ cogs/exceptionhandler.py
@@ -22,8 +22,14 @@ class ExceptionHandler(commands.Cog):
                                    err: discord.app_commands.AppCommandError):
         user = await self.bot.fetch_user(self.bot.owner_id)
         stacktrace = ''.join(traceback.format_exception(None, err, None))
-        msg = "Exception in command **/%s**\n```logs\n%s```"
-        await user.send(msg % (intr.command.name, stacktrace))
+        msg = "Exception in command **/%s** (%s)\n```logs\n%s```"
+
+        if not hasattr(intr.message, "jump_url"):
+            jump_url = "ephemeral"
+        else:
+            jump_url = intr.message.jump_url
+
+        await user.send(msg % (intr.command.name, jump_url, stacktrace))
 
         if intr.response.is_done():
             await intr.edit_original_response(content=ERROR_MSG)
