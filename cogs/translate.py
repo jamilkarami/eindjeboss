9bb38711412514a2631bae39b1ee3bf712a85083
Jamil <jamil_karami@hotmail.com>
2024-07-15 19:53:17 +0200

    Move translate feature from Google translate to DeepL

--- cogs/translate.py
+++ cogs/translate.py
@@ -1,27 +1,25 @@
 import logging as lg
 import os
 import re
-import uuid
 
+import deepl
 import discord
 from discord import app_commands
 from discord.ext import commands
-from googletrans import Translator
-from googletrans.constants import LANGUAGES
 
 from bot import Eindjeboss
-from util.vars.eind_vars import CHANNEL_IGNORE_LIST
+from util.vars.eind_vars import CHANNEL_IGNORE_LIST, DEEPL_LANGUAGE_CODES, DEEPL_LANGUAGE_NAMES
 
-translator = Translator()
+translator = deepl.Translator(os.getenv("DEEPL_API_KEY"))
 
 TRANSLATE_LANGUAGES = [
-    app_commands.Choice(name="English", value="english"),
-    app_commands.Choice(name="Dutch", value="dutch"),
-    app_commands.Choice(name="German", value="german"),
-    app_commands.Choice(name="Arabic", value="arabic"),
-    app_commands.Choice(name="French", value="french"),
-    app_commands.Choice(name="Spanish", value="spanish"),
-    app_commands.Choice(name="Esperanto", value="esperanto"),
+    app_commands.Choice(name="English", value="EN-GB"),
+    app_commands.Choice(name="Dutch", value="NL"),
+    app_commands.Choice(name="German", value="DE"),
+    app_commands.Choice(name="Arabic", value="AR"),
+    app_commands.Choice(name="French", value="FR"),
+    app_commands.Choice(name="Spanish", value="SP"),
+    app_commands.Choice(name="Italian", value="IT"),
 ]
 
 TRANSLATE_PROMPT_REGEX = r"(?:tr|translate) (.+) to (.+)"
@@ -31,10 +29,7 @@ class Translate(commands.Cog):
 
     def __init__(self, bot: Eindjeboss):
         self.bot = bot
-        self.ctx_menu = app_commands.ContextMenu(
-            name='Translate Message',
-            callback=self.translate_context,
-        )
+        self.ctx_menu = app_commands.ContextMenu(name='Translate Message', callback=self.translate_context)
         self.bot.tree.add_command(self.ctx_menu)
 
     @commands.Cog.listener()
@@ -50,67 +45,60 @@ class Translate(commands.Cog):
 
         message_content = msg.content.lower()
 
-        calc_pattern = re.compile(TRANSLATE_PROMPT_REGEX)
-        matches = calc_pattern.match(message_content)
+        translate_re = re.compile(TRANSLATE_PROMPT_REGEX)
+        matches = translate_re.match(message_content)
 
-        if (matches):
+        if matches:
             try:
                 req = matches.group(1)
                 lang = matches.group(2)
-                translated = TranslateUtil.translate_text(req, src='auto',
-                                                          dst=lang)
+                target_lang = DEEPL_LANGUAGE_NAMES.get(lang.lower())
+                if not target_lang:
+                    raise ValueError(f"Destination language **{lang}** not found.")
+                translated = TranslateUtil.translate_text(req, src=None, dst=target_lang)
             except ValueError as e:
                 name = msg.author.name
-                lg.error(f'Failed to translate \"{req}\" to {lang} for {name}')
+                lg.error(f"Failed to translate \"{req}\" to {lang} for {name}")
                 lg.debug(e)
                 await msg.reply('Destination language invalid. Check typos.')
             else:
-                await msg.reply(f"{translated.text}")
+                await msg.reply(f"{translated[0]}")
                 lg.info("Translated text for %s", msg.author.name)
 
-    async def translate_context(self, intr: discord.Interaction,
-                                msg: discord.Message):
+    async def translate_context(self, intr: discord.Interaction, msg: discord.Message):
         tr = TranslateUtil.translate_text(msg.content, None)
 
-        lang = LANGUAGES[tr.src]
+        src = f"**{msg.content}**"
+        dst = f"**{tr[0]}**"
+        lng = f"**{DEEPL_LANGUAGE_CODES.get(tr[1]).capitalize()}**"
 
-        translate_msg = "Translation for \"%s\" from %s:\n\n%s"
-        log_msg = "Sent translation to %s for message \"%s\" by %s"
+        await intr.response.send_message(content=f"Translation for {src} from {lng}: {dst}", ephemeral=True)
+        lg.info(f"Sent translation to {intr.user.name} for message {msg.content} by {msg.author.name}")
 
-        await intr.response.send_message(
-            content=translate_msg % (msg.content, lang.capitalize(), tr.text),
-            ephemeral=True)
-        lg.info(log_msg % (intr.user.name, msg.content, msg.author.name))
-
-    @commands.command(aliases=[])
-    async def tr(self, ctx):
-        await ctx.message.reply(
-            ["!tr is RIP. Please right-click/hold the message you wish to"
-             " you wish to translate, click on apps -> Translate Message"])
-
-    @app_commands.command(name="translate",
-                          description="Translate a specific text.")
+    @app_commands.command(name="translate", description="Translate a specific text.")
     @app_commands.choices(src=TRANSLATE_LANGUAGES, dst=TRANSLATE_LANGUAGES)
-    async def translate(self, intr: discord.Interaction,
-                        text: str,
-                        src: app_commands.Choice[str],
-                        dst: app_commands.Choice[str]):
-        tr = TranslateUtil.translate_text(text, src.value, dst.value)
-        await intr.response.send_message(
-            f"Translation of _\"{text}\"_ from"
-            f" _{src.name}_ to _{dst.name}_: {tr.text}")
-        lg.info("Translated text for %s", intr.user.name)
+    async def translate(self, intr: discord.Interaction, text: str,
+                        src: app_commands.Choice[str] = None, dst: app_commands.Choice[str] = None):
+        src_lang = src.value if src else None
+        dst_lang = dst.value if dst else "EN-GB"
+        tr = TranslateUtil.translate_text(text, src_lang, dst_lang)
+
+        res_src = DEEPL_LANGUAGE_CODES.get(tr[1]).capitalize()
+        res_dst = DEEPL_LANGUAGE_CODES.get(dst_lang).capitalize()
+
+        await intr.response.send_message(f"Translation of _\"{text}\"_ from _{res_src}_ to _{res_dst}_: {tr[0]}")
+        lg.info(f"Translated text for {intr.user.name}")
 
 
 class TranslateUtil:
     @staticmethod
     def translate_text(message, src, dst=None):
         if not dst:
-            dst = 'english'
-        translated = translator.translate(message) if not src \
-            else translator.translate(message, src=src, dest=dst)
+            dst = 'EN-GB'
+        translated = translator.translate_text(message, target_lang=dst) if not src \
+            else translator.translate_text(message, source_lang=src, target_lang=dst)
 
-        return translated
+        return translated.text, translated.detected_source_lang
 
     @staticmethod
     def cleanup(text: str):
